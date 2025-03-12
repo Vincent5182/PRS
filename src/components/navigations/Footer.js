285a5b0913ca21571e0f6cf07b4001f7a875c96b
Vincent5182 <46739285+Vincent5182@users.noreply.github.com>
2025-03-12 01:09:32 -0700

    added link

--- src/components/navigations/Footer.js
+++ src/components/navigations/Footer.js
@@ -5,37 +5,42 @@ import './Footer.css'
 const MenuBar = () => {
     return (
         <footer >
-               <section id="contact-us">
-            <div className="footer">
-                <div >
+            <section id="contact-us">
+                <div className="footer">
+                    <div >
+                        <p>
+                            San Joaquin County BHS Consumer Support Warm Line 24/7 | (209) 468-3585
+                        </p>
+                        <p>
+                            San Joaquin County Mental Health Crisis Line | (209) 468-8686
+                        </p>
+                    </div>
+                    <br>
+                    </br>
+                    <br>
+                    </br>
                     <p>
-                        San Joaquin County BHS Consumer Support Warm Line 24/7 | (209) 468-3585
+                        The Wellness Center
                     </p>
                     <p>
-                        San Joaquin County Mental Health Crisis Line | (209) 468-8686
+                        1109 N. California Street, Stockton, CA 95202-1537
+                    </p>
+                    <p>
+                        (209) 451-3977
+                    </p>
+                    <p>
+                        Fax line (209) 888-4798
+                    </p>
+                    <Link to="https://www.gofundme.com/f/supporttwcsjpeerrecoveryservices">
+                        <img alt="GoFundMe Logo" width={150} height={80} src={require('./../../images/gofundme.png')} />
+                    </Link>
+                    <p>Designed by
+                    ​
+                        <Link style={{ textDecoration: 'none', color: "white" }}  to="https://www.linkedin.com/in/vincentteas/">
+                            Vincent Teas
+                        </Link>
                     </p>
                 </div>
-                <br>
-                </br>
-                <br>
-                </br>
-                <p>
-                    The Wellness Center
-                </p>
-                <p>
-                    1109 N. California Street, Stockton, CA 95202-1537
-                </p>
-                <p>
-                    (209) 451-3977
-                </p>
-                <p>
-                    Fax line (209) 888-4798
-                </p>
-                <Link to="https://www.gofundme.com/f/supporttwcsjpeerrecoveryservices">
-                    <img alt="GoFundMe Logo" width={150} height={80} src={require('./../../images/gofundme.png')} />
-                </Link>
-                <p>Designed by Vincent Teas</p>
-            </div>
             </section>
         </footer>
     )
